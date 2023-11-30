516eb00d90550ba486311cf9d8c61c097b15aaf9
mafiavietquat <quochuy.4work@gmail.com>
2023-11-30 12:07:49 +0700

    edit msg in auth

--- Backend/src/auth/auth.py
+++ Backend/src/auth/auth.py
@@ -17,16 +17,16 @@ def authMiddleware(func):
         try:
             token = request.headers.get("Authorization")
             if not token:
-                return errorStatus.statusCode("Invalid Authentication.")
+                return errorStatus.statusCode("Invalid Authentication.",400)
 
             user = jwt.decode(token, ACCESS_TOKEN_SECRET, algorithms=["HS256"])
             setattr(request, 'user', user)
             return func(*args, **kwargs)
 
         except jwt.ExpiredSignatureError:
-            return errorStatus.statusCode("Token has expired.")
+            return errorStatus.statusCode("Token has expired.",400)
         except jwt.InvalidTokenError:
-            return errorStatus.statusCode("Invalid Authentication.")
+            return errorStatus.statusCode("Invalid Authentication.",400)
         except Exception as e:
             # return errorStatus.statusCode(str(e), 500)
             return str(e)
